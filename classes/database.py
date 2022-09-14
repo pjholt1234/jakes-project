4c621a4b535fb4dcaede329888c923f74f6d7994
PJ <pjholt1234@gmail.com>
2022-09-14 22:54:23 +0100

    basic database class tweaks

--- classes/database.py
+++ classes/database.py
@@ -1,29 +1,83 @@
-import mysql.connector
+import mysql.connector as mysql
 import os
 from dotenv import load_dotenv
+from os.path import exists
 
 class Database:
-    def __init__(self):
+    def __init__(self)  -> None:
+        self._db_host   = None
+        self._db_user   = None
+        self._db_password = None
+        self._db_name   = None
+        self._connected  = False
+
+
+        self._configCheck()
         self._db = self._connection()
-        self.connected = self._validateConnection()
 
-    def _connection(self):
+    def _configCheck(self):
         load_dotenv()
-        return mysql.connector.connect(
-            host=os.getenv('HOST', default=None),
-            user=os.getenv('DB_USER', default=None),
-            passwd=os.getenv('DB_PASSWORD', default=None),
-        )
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, '../.env')
 
-    def query(self, query):
-        mycursor = self._db.cursor()
-        mycursor.execute(query)
-        return mycursor.fetchall()
+        if(exists(filename) == False):
+            print('Missing ".env" File')
+            return False
 
-    def _validateConnection(self) -> bool:
+        if(os.getenv('HOST', default=None) == None):
+            print('Missing "HOST" env value')
+            return False
+
+        if(os.getenv('DB_USER', default=None) == None):
+            print('Missing "DB_USER" env value')
+            return False
+
+        if(os.getenv('DB_PASSWORD', default=None) == None):
+            print('Missing "DB_PASSWORD" env value')
+            return False
+
+        if(os.getenv('DB_NAME', default=None) == None):
+            print('Missing "DB_NAME" env value')
+            return False    
+
+        self._db_host       = os.getenv('HOST', default=None)
+        self._db_user       = os.getenv('DB_USER', default=None)
+        self._db_password   = os.getenv('DB_PASSWORD', default=None)
+        self._db_name       = os.getenv('DB_NAME', default=None)
+
         return True
 
+    def _connection(self):
+        try:
+            connection = mysql.connect(
+                host=self._db_host,
+                user=self._db_user,
+                password=self._db_password,
+                database=self._db_name
+            )
+        
+        except (mysql.Error, mysql.Warning) as e:
+            self._connected = False
+            print("Error while connecting to MySQL", e)
 
-db = Database()     
+        finally:
+            if connection.is_connected():
+                print('MySQL Connected')
+                self._connected = True
+                return connection
+    
 
-print(db.query("SHOW DATABASES LIKE 'test'"))
+    def query(self, query):
+        if(self._connected):
+            try:
+                mycursor = self._db.cursor()
+                mycursor.execute(query)
+            except (mysql.Error, mysql.Warning) as e:
+                print("Error while running query: '" + query + "': ", e)
+                return None
+            finally:
+                print('Fetching..')
+                return mycursor.fetchall()     
+        else:
+            print("No Database Connection")
+            return None
